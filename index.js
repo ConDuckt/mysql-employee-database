5caecd073d2b828e8ebf7ebdd9924e0767bf44fc
AMiro <aj.miro.b@gmail.com>
2023-04-16 19:11:59 -0500

    Add inquirer prompt with working example and structure for more

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 require("dotenv").config();
+const inquirer = require("inquirer");
 
 const { getAllDepartments } = require("./config/queries");
 
-getAllDepartments((err, results) => {
-    if (err) {
-    console.log(err);
-    } else {
-        console.log(results);
-    }
-});
+inquirer
+    .prompt({
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: [
+            "View all departments",
+            // "View all roles",
+            // "View all employees",
+            // "Add a department",
+            // "Add a role",
+            // "Add an employee",
+            // "Update an employee role",
+            "Quit"
+        ]
+    })
+    .then(answer => {
+        switch (answer.action) {
+        case "View all departments":
+            getAllDepartments((err, results) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.table(results);
+            }
+            });
+            break;
+        // case "View all roles":
+
+        //     break;
+        // case "View all employees":
+
+        //     break;
+        // case "Add a department":
+
+        //     break;
+        // case "Add a role":
+
+        //     break;
+        // case "Add an employee":
+
+        //     break;
+        // case "Update an employee role":
+
+        //     break;
+        case "Quit":
+            console.log("Goodbye!");
+            process.exit(0);
+        }
+  });
